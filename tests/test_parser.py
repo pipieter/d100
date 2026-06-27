64f495e523406fbc5ad3797b267a6e25a04de8ab
Pim Pieters <pim@pieters.tv>
2026-06-27 15:34:56 +0200

    Add parser tests

---
+++ tests/test_parser.py
@@ -0,0 +1,19 @@
+from d100 import parse
+import pytest
+
+from d100.errors import RollSyntaxError
+
+from d100.ast.unevaluated import ASTUnevaluated
+
+
+@pytest.mark.parametrize("expr", ["1d20abc", "xyz", "1d4 * 100gp", "x + y"])
+def test_unevaluated(expr: str):
+    # Test with not allowing unevaluated
+    with pytest.raises(RollSyntaxError):
+        parse(expr, allow_unevaluated=False)
+
+    # Test with allowing evaluated
+    parsed = parse(expr, allow_unevaluated=True)
+    nodes = parsed.flatten()
+    unevaluated = [node for node in nodes if isinstance(node, ASTUnevaluated)]
+    assert len(unevaluated) > 0
